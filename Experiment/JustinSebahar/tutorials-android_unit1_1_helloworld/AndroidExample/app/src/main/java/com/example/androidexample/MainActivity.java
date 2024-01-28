f6511e3ef00c133fa767b95998a33e4f5413afe8
Justin Sebahar <jtseb@iastate.edu>
2024-01-28 14:37:19 -0600

    Customized the android application for hello world

--- Experiment/JustinSebahar/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiment/JustinSebahar/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -13,6 +13,8 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
+    private Button bBlue; //Button to swap colors!
+    private Button bOrange; //Button to swap colors!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,25 @@ public class MainActivity extends AppCompatActivity {
         messageText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         messageText.setText("Hello World! Justin was here!");
 
+        //Instantiate buttons and change their colors
+        bBlue = findViewById(R.id.bBlue);
+        bBlue.setBackgroundColor(getColor(R.color.blue));
+        bOrange = findViewById(R.id.bOrange);
+        bOrange.setBackgroundColor(getColor(R.color.orange));
+
+        //Set action listeners so that when either button is clicked, the text changes to their color
+        bBlue.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                messageText.setTextColor(getColor(R.color.blue));
+            }
+        });
+
+        bOrange.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                messageText.setTextColor(getColor(R.color.orange));
+            }
+        });
     }
 }
