0f76f8929ba5043845167556eb97e6eba87b2772
collalti@iastate.edu <collalti>
2024-02-09 08:34:08 -0600

    Create templated for backend using SpringBoot Initializer and added code to ensure the new template works at a basic level

---
+++ Backend/BackendCode/src/main/java/MS3_3/Backend/ItineraryBuddyApplication.java
@@ -0,0 +1,13 @@
+package MS3_3.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraryBuddyApplication {
+
+	public static void main(String[] args) throws Exception {
+		//Runs on http://localhost:7070
+		SpringApplication.run(ItineraryBuddyApplication.class, args);
+	}
+}
---
+++ Backend/BackendCode/src/main/java/MS3_3/Backend/WelcomeController.java
@@ -0,0 +1,32 @@
+package MS3_3.Backend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+class WelcomeController {
+
+    //modified messages of the first two
+    @GetMapping("/")
+    public String welcome() {
+        return "MS3_3 welcomes you to COMS 309";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "MS3_3 welcomes " + name + " to CS309";
+    }
+
+    //Two new mappings
+    @GetMapping("/whatIs")
+    public String whatIs() {
+        return "COMS 309 is a project class at ISU";
+    }
+
+    @GetMapping("/whatIs/{name}")
+    public String whatIsName(@PathVariable String name) {
+        return name + " is curious about CS309";
+    }
+}
