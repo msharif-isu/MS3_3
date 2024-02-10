6a57880e5e5ba3c0c10213ce96d11cb1b427c9f4
Aina <qistina@iastate.edu>
2024-02-10 14:59:06 -0600

    Revert "Remove folder ItineraryBuddy"
    This reverts commit f0c920ede67ca8a842e43bfbd7d0f48207cadfc9.

---
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.itinerarybuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.*;
+import android.widget.*;
+import android.os.Bundle;
+
+/**
+ * This is the login page.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        test = findViewById(R.id.test_view);
+        test.setText("Test- this is the login page");
+    }
+}
