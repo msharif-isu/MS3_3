bb75350721bdff73fa37450ff1e8562ccab1f49c
Justin Sebahar <jtseb@iastate.edu>
2024-02-19 17:07:20 -0600

    Trying to resolve an error with new git branch

---
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/AdminDashboardActivity.java
@@ -0,0 +1,15 @@
+package com.example.itinerarybuddy;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/** Control panel where an admin will be able to moderate activity. */
+public class AdminDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_dashboard_activity);
+    }
+
+}
--- Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/LoginActivity.java
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/LoginActivity.java
@@ -11,14 +11,9 @@ import android.os.Bundle;
  */
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView test;
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-
-        test = findViewById(R.id.test_view);
-        test.setText("Test- this is the login page");
     }
 }
