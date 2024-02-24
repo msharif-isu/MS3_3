15c8940a33ecdd982726dbdf8f42241ded5080cc
Jackson Collalti <collalti@iastate.edu>
2024-02-24 13:30:04 -0600

    Update controller return types for better communication between frontend and backend, also deleted commented out code

--- Backend/BackendCode/src/main/java/MS3_3/Backend/UserTypes/User.java
+++ Backend/BackendCode/src/main/java/MS3_3/Backend/UserTypes/User.java
@@ -8,10 +8,6 @@ public class User {
 
     private String email;
 
-    private String firstName;
-
-    private String lastName;
-
     @Id
     private String userName;
 
@@ -29,13 +25,9 @@ public class User {
 
     private boolean canPost;
 
-    public User(String email,String firstName,String lastName,String userName,String password,String state,String city,
+    public User(String email, String userName,String password,String state,String city,
                 String userType){
         this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.password = password;
         this.state = state;
         this.city = city;
         this.userType = userType;
@@ -54,13 +46,6 @@ public class User {
         return userName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
 
     public void setPassword(String password) {
         this.password = password;
@@ -70,13 +55,6 @@ public class User {
         return password;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
 
     public void setEmail(String email) {
         this.email = email;
