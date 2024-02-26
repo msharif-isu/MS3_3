80a0e6f42fc3f6f2a8fd6184c6854af7d132d125
Justin Sebahar <jtseb@iastate.edu>
2024-02-26 14:44:24 +0000

    Revert "Delete MainActivity.java"
    This reverts commit 9f0b434fb6064b0824cb534d94c31aab1ad1dd95

---
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.itinerarybuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.*;
+import android.widget.*;
+import android.os.Bundle;
+
+/**
+ * This is the home page.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
