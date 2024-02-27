c30b62cf884c84a6c082e2201d59a09f9f727398
Jackson Collalti <collalti@iastate.edu>
2024-02-26 19:27:14 -0600

    Backend Login Resolving Merge Conflicts

--- Backend/BackendCode/src/main/java/MS3_3/Backend/ItineraryBuddyApplication.java
+++ Backend/BackendCode/src/main/java/MS3_3/Backend/ItineraryBuddyApplication.java
@@ -1,13 +1,19 @@
 package MS3_3.Backend;
 
+import MS3_3.Backend.UserTypes.User;
+import MS3_3.Backend.UserTypes.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ItineraryBuddyApplication {
 
 	public static void main(String[] args) throws Exception {
-		//Runs on http://localhost:7070
+		//Runs on http://localhost:8080
+		//Or http://coms-309-035.class.las.iastate.edu:8080/Users
 		SpringApplication.run(ItineraryBuddyApplication.class, args);
 	}
+
 }
