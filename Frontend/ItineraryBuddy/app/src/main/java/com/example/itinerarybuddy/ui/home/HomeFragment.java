b328ebba8263c7b4962b7c343a10222474053f57
Aina <qistina@iastate.edu>
2024-02-28 08:18:56 -0600

    Completed the mainpage with CRUDL

--- Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/ui/home/HomeFragment.java
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/ui/home/HomeFragment.java
@@ -1,24 +1,23 @@
 package com.example.itinerarybuddy.ui.home;
 
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.DatePicker;
-import android.widget.TextView;
+import android.widget.PopupMenu;
 
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.example.itinerarybuddy.data.Itinerary;
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -26,11 +25,9 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.sql.Time;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -55,8 +52,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import javax.xml.transform.ErrorListener;
-
 
 public class HomeFragment extends Fragment {
 
@@ -64,7 +59,6 @@ public class HomeFragment extends Fragment {
     private ArrayAdapter<String> itineraryAdapter;
     private EditText startDateInput;
     private EditText endDateInput;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -75,8 +69,6 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
-
        /* final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
 
@@ -84,7 +76,14 @@ public class HomeFragment extends Fragment {
         ListView list = root.findViewById(R.id.listViewItineraries);
         list.setAdapter(itineraryAdapter);
 
-        fetchItineraryInformation();
+        GET_itinerary();
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                showPopupMenu(view, position);
+            }
+        });
 
         FloatingActionButton fab = root.findViewById(R.id.addItinerary);
 
@@ -104,6 +103,8 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 
+
+    //POST (Add itinerary function related)
     private void showAddItineraryDialog() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
@@ -154,6 +155,7 @@ public class HomeFragment extends Fragment {
         builder.show();
     }
 
+
     private String generateUniqueTripCode() {
 
         Random rand = new Random(System.currentTimeMillis());
@@ -169,7 +171,6 @@ public class HomeFragment extends Fragment {
             }
         }
     }
-
 
     private void showDatePickerDialog(final EditText date) {
 
@@ -264,7 +265,7 @@ public class HomeFragment extends Fragment {
 
         try {
 
-            //Create a JSONOnject from the tripData
+            //Create a JSONObject from the tripData
             itineraryData = new JSONObject(tripData);
 
             //JsonObjectRequest for the POST request
@@ -277,12 +278,8 @@ public class HomeFragment extends Fragment {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.e("Volley Error: ", error.toString());
-                    // Add more detailed logging for better error analysis
-                    if (error.networkResponse != null) {
-                        Log.e("Volley Error", "Status Code: " + error.networkResponse.statusCode);
-                        Log.e("Volley Error: ", "Error Response: " + new String(error.networkResponse.data));
-                    }
+
+                    Log.e("Volley Error POST: ", error.toString());
                     Toast.makeText(requireContext(), "Error creating itinerary", Toast.LENGTH_SHORT).show();
                 }
             })
@@ -308,46 +305,276 @@ public class HomeFragment extends Fragment {
 
             //Add the JsonObjectRequest to the request queue
             Itinerary.requestQueue.add(jsonObject);
-            Singleton.getInstance(requireContext()).addRequest(jsonObject);
+           // Singleton.getInstance(requireContext()).addRequest(jsonObject);
 
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        //Saving the tripData locally
-        // saveItineraryToShared(destination, tripCode, startDate, endDate);
-    }
 
-
-
-    private void saveItineraryToShared(String destination, String tripCode, String startDate, String endDate) {
-
-        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("itinerary_preferences", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        //Create a unique key for each itinerary based on tripCode
-        String itineraryKey = "itinerary_" + tripCode;
-
-        String itineraryInfo =
-                "Destination: " + destination
-                        + "\nTrip Code: " + tripCode
-                        + "\nStart Date: " + startDate
-                        + "\nEnd Date: " + endDate;
-
-        editor.putString(itineraryKey, itineraryInfo);
-        editor.apply();
 
     }
 
 
-    public void fetchItineraryInformation(){
+    //PUT (Update and Edit itinerary function related)
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.itinerary_menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
-        loadSavedItineraries();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
 
-        String getItineraryUrl = "https://5569939f-7918-4af9-937a-86edcfe9bc7f.mock.pstmn.io/Itinerary/GetInfo";
+        if(item.getItemId() == R.id.action_edit){
+            return true;
+        }
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, getItineraryUrl, null,
+        else if(item.getItemId() == R.id.action_delete){
+            return true;
+        }
+
+        else{
+            return false;
+        }
+    }
+
+    private void showPopupMenu(View view, int position){
+
+        PopupMenu popupMenu = new PopupMenu(requireContext(), view);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+
+        inflater.inflate(R.menu.itinerary_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.action_edit){
+                    editItinerary(position);
+                    return true;
+                }
+
+                else if(item.getItemId() == R.id.action_delete){
+                    deleteItinerary(position);
+                    return true;
+                }
+
+                else{
+                    return false;
+                }            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void editItinerary(final int position){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Edit Itinerary");
+
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_add_itinerary, null);
+        builder.setView(dialogView);
+
+        final EditText destinationEdit = dialogView.findViewById(R.id.destinationEditText);
+        startDateInput = dialogView.findViewById(R.id.startDateEditText);
+        endDateInput = dialogView.findViewById(R.id.endDateEditText);
+
+        String itineraryInfo = itineraryAdapter.getItem(position);
+
+        if(itineraryInfo != null){
+
+            String[] itineraryData = itineraryInfo.split("\n");
+            destinationEdit.setText(itineraryData[0].substring("Destination: ".length()));
+            startDateInput.setText(itineraryData[2].substring("Start Date: ".length()));
+            endDateInput.setText(itineraryData[3].substring("End date: ".length()));
+
+        }
+
+        startDateInput.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                showDatePickerDialog(startDateInput);
+            }
+        });
+
+        endDateInput.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                showEndDatePickerDialog(Calendar.getInstance(), endDateInput);
+            }
+        });
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String destination = destinationEdit.getText().toString();
+                String startDate = startDateInput.getText().toString();
+                String endDate = endDateInput.getText().toString();
+
+                updateItinerary(position, destination, startDate, endDate);
+
+                PUT_itinerary(destination, position, startDate, endDate);
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void updateItinerary(int position, String destination, String startDate, String endDate){
+
+        String updatedItineraryInfo = "Destination: " + destination +
+                "\nTrip Code: " + getTripCodeFromAdapterPosition(position) +
+                "\nStart Date: " + startDate +
+                "\nEnd Date: " + endDate;
+
+        itineraryAdapter.remove(itineraryAdapter.getItem(position));
+        itineraryAdapter.insert(updatedItineraryInfo,0);
+        itineraryAdapter.notifyDataSetChanged();
+    }
+
+    private String getTripCodeFromAdapterPosition(int position){
+        String itineraryInfo = itineraryAdapter.getItem(position);
+
+        if(itineraryInfo != null){
+
+            String[] itineraryData = itineraryInfo.split("\n");
+            String tripCode = itineraryData[1].substring("Trip Code:".length());
+            return tripCode;
+        }
+
+        return null;
+    }
+
+    private void PUT_itinerary(final String destination, final int position, final String startDate, final String endDate){
+
+        String tripCode = getTripCodeFromAdapterPosition(position);
+        //String url = "http://coms-309-035.class.las.iastate.edu:8080/Itinerary/Update" + tripCode;
+        String url = "https://5569939f-7918-4af9-937a-86edcfe9bc7f.mock.pstmn.io/Itinerary/Update/" + tripCode;
+
+        StringRequest updateRequest = new StringRequest(Request.Method.PUT, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Update Response: ", response);
+                        Toast.makeText(requireContext(), "Itinerary updated successfully", Toast.LENGTH_SHORT).show();
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error PUT: ", error.toString());
+
+                        Toast.makeText(requireContext(), "Error updating itinerary", Toast.LENGTH_SHORT).show();
+                    }
+                }){
+
+            @Override
+            protected Map<String, String> getParams(){
+
+                Map<String, String> params = new HashMap<>();
+                params.put("destination", destination);
+                params.put("start date", startDate);
+                params.put("end date", endDate);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders(){
+
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                return headers;
+            }
+        };
+
+        Itinerary.requestQueue.add(updateRequest);
+    }
+
+
+    //DELETE (Delete itinerary function related)
+    private void DELETE_itinerary(final String tripCode) {
+
+        //String url = "http://coms-309-035.class.las.iastate.edu:8080/Itinerary/Delete" + tripCode;
+        String url = "https://5569939f-7918-4af9-937a-86edcfe9bc7f.mock.pstmn.io/Itinerary/Delete/" + tripCode;
+
+        StringRequest deleteRequest = new StringRequest(Request.Method.DELETE, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Delete Response: ", response);
+                        Toast.makeText(requireContext(), "Itinerary deleted successfully", Toast.LENGTH_SHORT).show();
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error Delete:", error.toString());
+
+                        Toast.makeText(requireContext(), "Error deleting itinerary", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            public Map<String, String> getHeaders() {
+                // Add headers if needed, e.g., authorization token
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                return headers;
+            }
+        };
+
+        Itinerary.requestQueue.add(deleteRequest);
+    }
+
+
+    private void deleteItinerary(final int position){
+
+        AlertDialog.Builder confirmDeleteBuilder = new AlertDialog.Builder(requireContext());
+        confirmDeleteBuilder.setTitle("Confirm Delete");
+        confirmDeleteBuilder.setMessage("Are you sure you want to delete this itinerary?");
+
+        confirmDeleteBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+               String tripCode = getTripCodeFromAdapterPosition(position);
+
+               //Remove the data from the adapter
+                itineraryAdapter.remove(itineraryAdapter.getItem(position));
+                itineraryAdapter.notifyDataSetChanged();
+
+                DELETE_itinerary(tripCode);
+            }
+        });
+
+        confirmDeleteBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        confirmDeleteBuilder.show();
+    }
+
+    //GET (fetch itinerary information function related)
+
+    public void GET_itinerary(){
+
+
+        //String url = "http://coms-309-035.class.las.iastate.edu:8080/Itinerary/GetInfo";
+        String url = "https://5569939f-7918-4af9-937a-86edcfe9bc7f.mock.pstmn.io/Itinerary/GetInfo";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
@@ -360,11 +587,11 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void onErrorResponse(VolleyError error) {
 
-                        Log.e("Volley Error: ", error.toString());
+                        Log.e("Volley Error GET: ", error.toString());
 
                         if(error.networkResponse != null){
-                        Log.e("Volley Error", "Status Code: " + error.networkResponse.statusCode);
-                        Log.e("Volley Error: ", "Error Response: " + new String(error.networkResponse.data));
+                            Log.e("Volley Error", "Status Code: " + error.networkResponse.statusCode);
+                            Log.e("Volley Error: ", "Error Response: " + new String(error.networkResponse.data));
                         }
                         Toast.makeText(requireContext(), "Error fetching itinerary info", Toast.LENGTH_SHORT).show();
                     }
@@ -373,34 +600,24 @@ public class HomeFragment extends Fragment {
         Itinerary.requestQueue.add(jsonArrayRequest);
     }
 
-    private void loadSavedItineraries() {
-        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("itinerary_preferences", Context.MODE_PRIVATE);
+    private void updateItineraryAdapter(JSONArray itineraryArray) {
 
-        //Iterate through saved itineraries and update the adapter
-        Map<String, ?> allEntries = sharedPreferences.getAll();
+        //itineraryAdapter.clear();
 
-        for(Map.Entry<String, ?> entry:allEntries.entrySet()){
-
-            String itineraryInfo = entry.getValue().toString();
-            itineraryAdapter.insert(itineraryInfo,0);
-        }
-    }
-
-       private void updateItineraryAdapter(JSONArray itineraryArray) {
         for (int i = 0; i < itineraryArray.length(); i++) {
             try {
                 JSONObject itineraryObject = itineraryArray.getJSONObject(i);
                 String destination = itineraryObject.getString("destination");
                 String tripCode = itineraryObject.getString("tripCode");
-                String startDate = itineraryObject.getString("start date");  // Corrected key
-                String endDate = itineraryObject.getString("end date");      // Corrected key
+                String startDate = itineraryObject.getString("start date");
+                String endDate = itineraryObject.getString("end date");
 
                 String itineraryInfo = "Destination: " + destination +
                         "\nTrip Code: " + tripCode +
                         "\nStart Date: " + startDate +
                         "\nEnd Date: " + endDate;
 
-                itineraryAdapter.insert(itineraryInfo, 0);
+                itineraryAdapter.add(itineraryInfo);
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -409,6 +626,5 @@ public class HomeFragment extends Fragment {
 
         itineraryAdapter.notifyDataSetChanged();
     }
-
 
 }
