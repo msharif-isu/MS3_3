76476135b044000dcd9cb3efe3dfaac6426f6da7
Justin Sebahar <jtseb@iastate.edu>
2024-03-22 08:54:19 -0500

    [wip]

--- Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/ui/dashboard/GroupChatActivity.java
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/ui/dashboard/GroupChatActivity.java
@@ -47,7 +47,7 @@ public class GroupChatActivity extends AppCompatActivity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
