86eaf62f4c8ab89157d80a18e5b0cd204a13fe5a
collalti <collalti@iastate.edu>
2024-04-20 15:35:11 -0500

    [WIP] Working Put and Correct integration with group, still need to work on delete mapping which will just reset the the itinerary to the defualt state it was in at creation

--- Backend/BackendCode/src/main/java/MS3_3/Backend/TravelGroupItinerary/TravelGroupItineraryController.java
+++ Backend/BackendCode/src/main/java/MS3_3/Backend/TravelGroupItinerary/TravelGroupItineraryController.java
@@ -3,12 +3,8 @@ package MS3_3.Backend.TravelGroupItinerary;
 import MS3_3.Backend.Day.Day;
 import MS3_3.Backend.Groups.TravelGroup;
 import MS3_3.Backend.Groups.TravelGroupRepository;
-//import MS3_3.Backend.TravelGroupItinerary.Days.TravelGroupItineraryDay;
-//import MS3_3.Backend.TravelGroupItinerary.Days.TravelGroupItineraryDayRepository;
 import MS3_3.Backend.TravelGroupItinerary.Events.TravelGroupItineraryEvent;
 import MS3_3.Backend.TravelGroupItinerary.Events.TravelGroupItineraryEventRepository;
-import MS3_3.Backend.TravelGroupItinerary.Schedule.TravelGroupItinerarySchedule;
-import MS3_3.Backend.TravelGroupItinerary.Schedule.TravelGroupItineraryScheduleRepository;
 import jakarta.persistence.Access;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,14 +21,9 @@ public class TravelGroupItineraryController {
     @Autowired
     TravelGroupItineraryRepository travelGroupItineraryRepository;
 
-    //@Autowired
-    //TravelGroupItineraryDayRepository travelGroupItineraryDayRepository;
-
     @Autowired
     TravelGroupItineraryEventRepository travelGroupItineraryEventRepository;
 
-    @Autowired
-    TravelGroupItineraryScheduleRepository travelGroupItineraryScheduleRepository;
 
     @PostMapping("/Group/Itinerary")
     public TravelGroupItinerary createTravelGroupItinerary(@RequestBody TravelGroupItinerary travelGroupItinerary) {
@@ -113,63 +104,56 @@ public class TravelGroupItineraryController {
         existingItinerary.setStartDate(updatedItinerary.getStartDate());
         existingItinerary.setEndDate(updatedItinerary.getEndDate());
         existingItinerary.setNumDays(updatedItinerary.getNumDays());
-        existingItinerary.setTravelGroupItinerarySchedule(updatedItinerary.getTravelGroupItinerarySchedule());
+
+        // Clear the existing events list
+        existingItinerary.getTravelGroupItineraryEventsList().clear();
+
+        // Add the updated events to the existing itinerary
+        for (TravelGroupItineraryEvent updatedEvent : updatedItinerary.getTravelGroupItineraryEventsList()) {
+            TravelGroupItineraryEvent newEvent = new TravelGroupItineraryEvent(existingItinerary,
+                    updatedEvent.getDayNumber(), updatedEvent.getTime(),
+                    updatedEvent.getPlace(), updatedEvent.getNotes());
+            existingItinerary.getTravelGroupItineraryEventsList().add(newEvent);
+        }
+
+        // Save the updated itinerary
         TravelGroupItinerary output = travelGroupItineraryRepository.save(existingItinerary);
-        return travelGroupItineraryRepository.findByTravelGroupItineraryId(groupId);
+        return output;
     }
 
 
 
 
 
-    /**
+    /** Working Constant add
      @PutMapping("/Group/Itinerary/{groupId}")
-     public TravelGroup updateTravelGroupItinerary(@PathVariable int groupId,
+     public TravelGroupItinerary updateTravelGroupItinerary(@PathVariable int groupId,
      @RequestBody TravelGroupItinerary updatedItinerary) {
+
      // Retrieve the corresponding TravelGroup from the repository
-     TravelGroup travelGroup = travelGroupRepository.findByTravelGroupId(groupId);
+     TravelGroupItinerary existingItinerary = travelGroupItineraryRepository.findByTravelGroupItineraryId(groupId);
 
-     // Get the existing TravelGroupItinerary
-     TravelGroupItinerary existingItinerary = travelGroup.getTravelGroupItinerary();
-
+     int oldLength = existingItinerary.getTravelGroupItineraryEventsList().size();
      // Update the existing itinerary with the values from the updated itinerary
      existingItinerary.setItineraryName(updatedItinerary.getItineraryName());
      existingItinerary.setStartDate(updatedItinerary.getStartDate());
      existingItinerary.setEndDate(updatedItinerary.getEndDate());
      existingItinerary.setNumDays(updatedItinerary.getNumDays());
+     existingItinerary.getTravelGroupItineraryEventsList().clear();
 
-     // Clear existing days in the itinerary schedule
-     existingItinerary.getTravelGroupItinerarySchedule().getDays().clear();
+     List<TravelGroupItineraryEvent> temp = existingItinerary.getTravelGroupItineraryEventsList();
 
-     // Copy days and events from the updated itinerary to the existing itinerary's schedule
-     List<TravelGroupItineraryDay> updatedDays = updatedItinerary.getTravelGroupItinerarySchedule().getDays();
-     List<TravelGroupItineraryDay> newDays = updatedDays.stream()
-     .map(updatedDay -> {
-     TravelGroupItineraryDay newDay = new TravelGroupItineraryDay();
-     List<TravelGroupItineraryEvent> updatedEvents = updatedDay.getEvents();
-     List<TravelGroupItineraryEvent> newEvents = updatedEvents.stream()
-     .map(updatedEvent -> {
-     TravelGroupItineraryEvent newEvent = new TravelGroupItineraryEvent();
-     newEvent.setTime(updatedEvent.getTime());
-     newEvent.setPlace(updatedEvent.getPlace());
-     newEvent.setNotes(updatedEvent.getNotes());
-     return newEvent;
-     })
-     .collect(Collectors.toList());
-     newDay.setEvents(newEvents);
-     return newDay;
-     })
-     .collect(Collectors.toList());
+     for (int i = 0; i < updatedItinerary.getTravelGroupItineraryEventsList().size(); i++) {
+     temp.add(new TravelGroupItineraryEvent(existingItinerary,updatedItinerary.getTravelGroupItineraryEventsList().get(i).getDayNumber(),
+     updatedItinerary.getTravelGroupItineraryEventsList().get(i).getPlace(),updatedItinerary.getTravelGroupItineraryEventsList().get(i).getPlace(),
+     updatedItinerary.getTravelGroupItineraryEventsList().get(i).getNotes()));
+     existingItinerary.getTravelGroupItineraryEventsList().add(temp.get(i));
+     }
 
-     existingItinerary.getTravelGroupItinerarySchedule().setDays(newDays);
+     existingItinerary.setTravelGroupItineraryEventsList(temp);
 
-     // Update the itinerary in the travel group
-     travelGroup.setTravelGroupItinerary(existingItinerary);
-
-     // Save the updated TravelGroup
-     travelGroupRepository.save(travelGroup);
-
-     return travelGroup;
+     TravelGroupItinerary output = travelGroupItineraryRepository.save(existingItinerary);
+     return travelGroupItineraryRepository.findByTravelGroupItineraryId(groupId);
      }
      */
 
