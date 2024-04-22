19e513d0102ef8ff5c776c0b36e6136ec1b868a8
Justin Sebahar <jtseb@iastate.edu>
2024-04-22 11:35:25 -0500

    Finished my 4 test cases for miniassignment

--- Frontend/ItineraryBuddy/app/src/androidTest/java/com/example/itinerarybuddy/JustinSystemTest.java
+++ Frontend/ItineraryBuddy/app/src/androidTest/java/com/example/itinerarybuddy/JustinSystemTest.java
@@ -1,9 +1,11 @@
 package com.example.itinerarybuddy;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
@@ -25,9 +27,11 @@ import com.example.itinerarybuddy.ui.home.LoadGroup;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.AlertDialogLayout;
@@ -38,15 +42,18 @@ import androidx.test.rule.ActivityTestRule;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 @LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JustinSystemTest {
 
     private static final int DELAY_MS = 1000;
+
+    private static final String id = "10";
 
     @Rule   // needed to launch the activity
     public ActivityTestRule<LoginActivity> activityRule = new ActivityTestRule<>(LoginActivity.class);
 
     @Before
-    public void setup(){
+    public void setup() {
         String username = "Justin";
         String password = "1";
         onView(withId(R.id.username_input)).perform(typeText(username), closeSoftKeyboard());
@@ -59,13 +66,13 @@ public class JustinSystemTest {
     }
 
     @Test
-    public void loginTest(){
-        // Assert that the intended user has been logged in
+    public void test1Login() {
+        // Assert that the intended user has been logged in upon setup()
         assertEquals("Justin", UserData.getUsername());
     }
 
     @Test
-    public void joinGroupTest(){
+    public void test2JoinGroup() {
         onView(withId(R.id.group_button)).perform(click());
         try {
             Thread.sleep(DELAY_MS);
@@ -74,7 +81,7 @@ public class JustinSystemTest {
 
         int start = ListGroups.getAdapter().getCount();
         onView(withId(R.id.add_group_button)).perform(click());
-        onView(withId(R.id.group_code_input)).perform(typeText("10"), closeSoftKeyboard());
+        onView(withId(R.id.group_code_input)).perform(typeText(id), closeSoftKeyboard());
         onView(withId(android.R.id.button1)).perform(click());
         try {
             Thread.sleep(DELAY_MS);
@@ -83,11 +90,54 @@ public class JustinSystemTest {
         int end = ListGroups.getAdapter().getCount();
 
         //onView(withText("Test99\nDestination: Canada")).check(matches(isDisplayed()));
+
         assertEquals(start + 1, end);
     }
 
     @Test
-    public void leaveGroupTest(){
+    public void test3EditGroup() {
+        onView(withId(R.id.group_button)).perform(click());
+        try {
+            Thread.sleep(DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        // Make edits to group
+        onView(withText("Test99\nDestination: Canada")).perform(click());
+        try {
+            Thread.sleep(DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+        onView(withId(R.id.options_button)).perform(click());
+        onView(withText("Edit Travel Group")).perform(click());
+        onView(withId(R.id.group_name_input)).perform(clearText(), closeSoftKeyboard());
+        onView(withId(R.id.group_name_input)).perform(typeText("System Test"), closeSoftKeyboard());
+        onView(withId(android.R.id.button1)).perform(click());
+
+        try {
+            Thread.sleep(DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        // Verify edits worked
+        onView(withText("System Test\nDestination: Canada")).perform(click());
+        onView(withId(R.id.group_title)).check(matches(withText("System Test")));
+
+        // Restore group to original state
+        onView(withId(R.id.options_button)).perform(click());
+        onView(withText("Edit Travel Group")).perform(click());
+        onView(withId(R.id.group_name_input)).perform(clearText(), closeSoftKeyboard());
+        onView(withId(R.id.group_name_input)).perform(typeText("Test99"), closeSoftKeyboard());
+        onView(withId(android.R.id.button1)).perform(click());
+
+        try {
+            Thread.sleep(DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    @Test
+    public void test4LeaveGroup() {
         onView(withId(R.id.group_button)).perform(click());
         try {
             Thread.sleep(DELAY_MS);
@@ -111,9 +161,9 @@ public class JustinSystemTest {
         assertEquals(start - 1, end);
     }
 
-    /*
+        /*
     @Test
-    public void createGroupTest(){
+    public void test2CreateGroup(){
         onView(withId(R.id.group_button)).perform(click());
         try {
             Thread.sleep(DELAY_MS);
@@ -136,5 +186,4 @@ public class JustinSystemTest {
         assertEquals(start + 1, end);
     }
     */
-
 }
