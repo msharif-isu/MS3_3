6008c02ad2b2de9287c2b01a3ac5d8cdf932626b
Aina <qistina@iastate.edu>
2024-04-24 16:05:56 -0500

    First test case: testShowAddItineraryDialog

--- Frontend/ItineraryBuddy/app/src/androidTest/java/com/example/itinerarybuddy/AinaSystemTest.java
+++ Frontend/ItineraryBuddy/app/src/androidTest/java/com/example/itinerarybuddy/AinaSystemTest.java
@@ -1,5 +1,12 @@
 package com.example.itinerarybuddy;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.filters.LargeTest;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
@@ -7,6 +14,7 @@ import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import com.example.itinerarybuddy.ui.home.HomeFragment;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
@@ -20,5 +28,14 @@ public class AinaSystemTest {
 
     }
 
- 
+    @Test
+    public void testShowAddItineraryDialog() {
+        // Click on the "Add Itinerary" button
+        onView(withId(R.id.addItinerary)).perform(click());
+
+        // Verify that the "Add Itinerary" dialog is displayed
+        onView(withText("Add Itinerary")).check(matches(isDisplayed()));
+    }
+
+
 }
