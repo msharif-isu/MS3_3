93377c54f3683ddfe895b49c053082db0adc0581
collalti <collalti@iastate.edu>
2024-04-24 11:24:28 -0500

    [WIP] continued progress on the image service or blog images and starting work on blog controller

--- Backend/BackendCode/src/main/java/MS3_3/Backend/AmbassadorBlogPost/BlogPostController.java
+++ Backend/BackendCode/src/main/java/MS3_3/Backend/AmbassadorBlogPost/BlogPostController.java
@@ -1,4 +1,80 @@
 package MS3_3.Backend.AmbassadorBlogPost;
 
+import MS3_3.Backend.AmbassadorBlogPost.Images.BlogPostImageRepository;
+import MS3_3.Backend.AmbassadorBlogPost.Images.BlogPostStorageService;
+import MS3_3.Backend.FileUpload.Image;
+import MS3_3.Backend.Groups.TravelGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
 public class BlogPostController {
+
+    @Autowired
+    private BlogPostStorageService service;
+
+    @Autowired
+    private TravelGroupRepository travelGroupRepository;
+
+    @Autowired
+    private BlogPostImageRepository imageRepository;
+
+    @PostMapping("/Image")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        String uploadImage = service.uploadImage(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/Image/{Id}")
+    public ResponseEntity<?> downloadImageByName(@PathVariable int Id){
+        byte[] imageData=service.downloadImageByImageId(Id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageData);
+    }
+/**
+    @PostMapping("/Group/Image/{groupId}")
+    public ResponseEntity<?> uploadGroupImage(@PathVariable int groupId, @RequestParam("image")MultipartFile file) throws IOException {
+        String uploadImage = service.uploadGroupImage(file, groupId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/Group/Image/{groupId}")
+    public ResponseEntity<?> downloadGroupImage(@PathVariable int groupId){
+        byte[] imageData=service.downloadImageByGroupId(groupId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageData);
+    }
+
+    @PutMapping("/Group/Image/{groupId}")
+    public ResponseEntity<?> changeGroupImage(@PathVariable int groupId, @RequestParam("image")MultipartFile file) throws IOException {
+        String uploadImage = service.changeGroupImage(file, groupId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @DeleteMapping("/Group/Image/{groupId}")
+    public ResponseEntity<?> deleteGroupImage(@PathVariable int groupId) {
+        Image copy = new Image();
+        copy.setImageData(imageRepository.findById(38).getImageData());
+        copy.setType(imageRepository.findById(38).getType());
+        copy.setName(imageRepository.findById(38).getName());
+        imageRepository.save(copy);
+        String uploadImage = service.changeImageByGroupId(groupId,copy);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+    */
+
+
+
 }
