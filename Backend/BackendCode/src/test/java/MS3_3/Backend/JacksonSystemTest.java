9c3e0145c9e113c9b2d93208ad1dc892e9b5f392
collalti <collalti@iastate.edu>
2024-04-24 13:00:43 -0500

    Added 3 more test cases and coverage report

--- Backend/BackendCode/src/test/java/MS3_3/Backend/JacksonSystemTest.java
+++ Backend/BackendCode/src/test/java/MS3_3/Backend/JacksonSystemTest.java
@@ -1,6 +1,7 @@
 package MS3_3.Backend;
 
 import MS3_3.Backend.AdminDashboard.Admin;
+import MS3_3.Backend.AdminDashboard.AdminRepository;
 import MS3_3.Backend.Ambassador.Ambassador;
 import MS3_3.Backend.Ambassador.AmbassadorController;
 import MS3_3.Backend.Ambassador.AmbassadorRepository;
@@ -31,6 +32,9 @@ public class JacksonSystemTest {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    AdminRepository adminRepository;
 
     @Autowired
     AmbassadorRepository ambassadorRepository;
@@ -77,6 +81,22 @@ public class JacksonSystemTest {
     }
 
     @Test
+    public void testLogin() {
+        // Arrange
+        String userName = "UserName";
+        String password = "password";
+
+        // Act
+        ResponseEntity<User> response = restTemplate.getForEntity("/Users/login/{userName}/{password}", User.class, userName, password);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getUserName()).isEqualTo(userName);
+        assertThat(response.getBody().getPassword()).isEqualTo(password);
+    }
+
+    @Test
     public void testGroupPostNonAmbassador() {
         String travelDestination = "Europe";
         User user = new User("email",  "NotAmbassador",  "password",  "state",  "city",
@@ -107,41 +127,64 @@ public class JacksonSystemTest {
             e.printStackTrace();
         }
     }
-/**
+
     @Test
-    public void testGrantAmbassador() {
+    public void testGrantAdmin() {
         // Arrange
         String adminUserName = "admin";
         User user = new User("email",  "userName",  "password",  "state",  "city",
                 "User");
+        User user2 = new User("email",  "userName2",  "password2",  "state",  "city",
+                "User");
+        user.setUserType("Admin");
         Admin admin = new Admin(user);
-
-        ResponseEntity<Ambassador> response = restTemplate.put("/Ambassador/Grant/{adminUserName}/{userName}", user, User.class, admin, Admin.class);
+        adminRepository.save(admin);
+        userRepository.save(user);
+        userRepository.save(user2);
+        restTemplate.put("/Admin/Grant/{adminUserName}/{userName}", Admin.class, admin.getUserName(), user2.getUserName());
+        ResponseEntity<User> response = restTemplate.getForEntity("/Users/{userName}", User.class, user2.getUserName());
 
         // Assert
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().getUserType()).isEqualTo("Ambassador");
+        assertThat(response.getBody().getUserType()).isEqualTo("Admin");
 
     }
 
-@Test
-public void testGrantAmbassador() {
-    // Arrange
-    String UserName = "userName";
-    String Password = "123";
-    User user = new User("email", UserName, Password, "state", "city",
-            "User");
+    @Test
+    public void testRevokeAmbassador() {
+        User user = new User("email",  "userName",  "password",  "state",  "city",
+                "User");
+        User user2 = new User("email",  "userName2",  "password2",  "state",  "city",
+                "User");
+        user.setUserType("Admin");
+        Admin admin = new Admin(user);
+        Ambassador ambassador = new Ambassador(user2);
+        user2.setUserType("Ambassador");
+        ambassadorRepository.save(ambassador);
+        adminRepository.save(admin);
+        userRepository.save(user);
+        userRepository.save(user2);
+        restTemplate.put("/Ambassador/Revoke/{adminUserName}/{userName}", Admin.class, admin.getUserName(), user2.getUserName());
+        ResponseEntity<User> response = restTemplate.getForEntity("/Users/{userName}", User.class, user2.getUserName());
 
-
-    ResponseEntity<User> response = restTemplate.get("/Users/login/{userName}/{password}", UserName, Password, User.class);
-
-    // Assert
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(response.getBody()).isNotNull();
-    assertThat(response.getBody().getUserType()).isEqualTo("Ambassador");
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getUserType()).isEqualTo("User");
     }
-*/
+/**
+    @Test
+    public void testImageUpload() {
+        try {
+            ResponseEntity<Image> response = restTemplate.getForEntity("/Group/Image/{groupId}", Image.class, 1);
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(response.getBody().getName()).isEqualTo(null);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    */
 }
 
 
