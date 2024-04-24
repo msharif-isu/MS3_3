5ef32e818966f91021ddc13a142f8e68227587e2
Aina <qistina@iastate.edu>
2024-04-24 17:05:35 -0500

    Fourth test case: testDeleteItinerary

--- Frontend/ItineraryBuddy/app/src/androidTest/java/com/example/itinerarybuddy/AinaSystemTest.java
+++ Frontend/ItineraryBuddy/app/src/androidTest/java/com/example/itinerarybuddy/AinaSystemTest.java
@@ -4,6 +4,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
@@ -94,6 +95,54 @@ public class AinaSystemTest {
         onView(withText(containsString("Paris"))).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void testDeleteItinerary(){
+        onView(withId(R.id.addItinerary)).perform(click());
+
+        // Enter destination
+        onView(withId(R.id.destinationEditText))
+                .perform(typeText("Japan"), closeSoftKeyboard());
+
+        // Set start date to today
+        onView(withId(R.id.startDateEditText))
+                .perform(click());
+        // Set date and click OK
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(2024, 4, 22)); // Set your desired date
+        onView(withText("OK")).perform(click());
+
+        // Wait for end date EditText to become visible
+        onView(withId(R.id.endDateEditText)).check(matches(isDisplayed()));
+
+        // Set end date to two days later
+        onView(withId(R.id.endDateEditText))
+                .perform(click());
+        // Set date and click OK
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(2024, 4, 28)); // Set your desired date
+        onView(withText("OK")).perform(click());
+
+        // Click on continue
+        onView(withText("Continue")).perform(click());
+
+        onView(withId(R.id.iconPopUp)).perform(click());
+
+        // Now, let's check if the popup menu is displayed
+        onView(withText("Edit")).check(matches(isDisplayed()));
+        onView(withText("Delete")).check(matches(isDisplayed()));
+
+        // Click on "Delete" in the popup menu
+        onView(withText("Delete")).perform(click());
+
+        // Now, let's check if the delete confirmation dialog is displayed
+        onView(withText("Confirm Delete")).check(matches(isDisplayed()));
+
+        // Click on "Yes" to confirm deletion
+        onView(withText("Yes")).perform(click());
+
+        // Now, let's check if the itinerary is deleted from the list
+        onView(withText(containsString("Destination: Japan"))).check(doesNotExist());
+    }
 
 
 }
