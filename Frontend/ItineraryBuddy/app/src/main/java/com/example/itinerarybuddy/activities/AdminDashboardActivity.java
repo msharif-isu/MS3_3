ecd48a54c6a836b4f7b8c518b02a6f1da6a53df5
Justin Sebahar <jtseb@iastate.edu>
2024-04-29 15:19:51 -0500

    final visual

--- Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/activities/AdminDashboardActivity.java
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/activities/AdminDashboardActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -39,6 +41,9 @@ public class AdminDashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
         setContentView(R.layout.admin_dashboard_activity);
 
         Button button1;
--- Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/activities/DayCard.java
+++ Frontend/ItineraryBuddy/app/src/main/java/com/example/itinerarybuddy/activities/DayCard.java
@@ -1,6 +1,8 @@
 package com.example.itinerarybuddy.activities;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -32,6 +34,9 @@ public class DayCard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
         setContentView(R.layout.activity_day_card);
 
         // Retrieve the number of days from the intent extra
